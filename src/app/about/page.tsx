f8a8f88a19e90cd963c043b13d4fb1331b9017c2
LordHarsh <harshhbanka321@gmail.com>
2024-01-12 02:47:52 +0530

    feat add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <h3>Lights, camera, community!</h3>
+      <p>
+        Welcome to your cinematic playground. We&apos;re not just an IMDb clone,
+        we&apos;re a vibrant movie haven for cinephiles like you. Dive into a
+        colossal database, unleash your inner critic in our forums, and discover
+        hidden gems with personalized recommendations. Join the conversation,
+        stay informed, and celebrate the magic of cinema. Grab your popcorn, dim
+        the lights, and let&apos;s explore!
+      </p>
+      <p>
+        P.S. We&apos;re constantly evolving, so your feedback is always welcome.
+        Let&apos;s make this platform the ultimate online destination for movie
+        lovers!
+      </p>
+      <p>
+        This version retains the key points while being more concise and punchy.
+        You can further adjust the length by trimming specific sections or
+        sentences. Remember, the key is to convey the essence of your website
+        and entice users to explore further.
+      </p>
+    </div>
+  );
+}
