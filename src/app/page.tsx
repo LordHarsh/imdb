8c7453b1e169f8a6ac4d75a6ee634f8ad2381bb9
LordHarsh <harshhbanka321@gmail.com>
2024-01-12 02:25:31 +0530

    feat add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <h1 className="text-red-400">Home</h1>
     </div>
   );
