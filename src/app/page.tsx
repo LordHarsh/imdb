b18bd6fd72091865d83340d54430a1eb901ca127
LordHarsh <harshhbanka321@gmail.com>
2024-01-12 01:20:02 +0530

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <div>
+      <Header />
+      <h1 className="text-red-400">Home</h1>
+    </div>
+  );
 }
