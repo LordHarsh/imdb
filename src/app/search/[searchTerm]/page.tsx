966496ae3d549dfcccaec64d93e6fd396f5c1fe7
LordHarsh <harshhbanka321@gmail.com>
2024-01-13 01:42:43 +0530

    fix: change in param typescript

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -1,6 +1,6 @@
 import Results from "@/components/Results";
 
-export default async function SearchPage({ params }) {
+export default async function SearchPage({ params }: { params: any }) {
   const res = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&include_adult=false`
   );
