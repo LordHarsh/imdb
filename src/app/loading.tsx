f13c62ae6088d1198ae64c61514e89fd02195d68
LordHarsh <harshhbanka321@gmail.com>
2024-01-12 22:43:05 +0530

    feat: add card components

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='flex justify-center'>
-        <Image className='h-96' src="spinner.svg" alt="loading..." />
+        <Image className='h-96 w-96' width={500} height={500}  src="spinner.svg" alt="loading..." />
     </div>
   )
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Results({ results }: { results: any }) {
-  return <div>
-    {results.map((result: any) => (
+  return (
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto p-4">
+      {results.map((result: any) => (
         <div key={result.id}>
-            {/* <img src={result.poster_path} alt="poster" /> */}
-            <p>{result.original_name || result.original_title}</p>
+          {/* <img src={result.poster_path} alt="poster" /> */}
+          {/* <p>{result.original_name || result.original_title}</p> */}
+          <Card key={result.id} result={result} />
         </div>
-    ))}
-  </div>;
+      ))}
+    </div>
+  );
 }
